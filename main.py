3b816fec1c9ba147eb9a7d9029caa00d69fee0ac
Leo T <leo.trygg@elev.ga.ntig.se>
2024-01-25 10:44:46 +0100

    slay

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def main():
-    print("wow, this is a cool project!")
+from flask import Flask, render_template
 
-if __name__ == "__main__":
-    main()
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('website.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
