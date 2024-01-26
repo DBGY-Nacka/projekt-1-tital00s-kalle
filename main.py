fc00991fa4000182aeb60ee226a09f599bb29199
Leo T <leo.trygg@elev.ga.ntig.se>
2024-01-26 16:44:48 +0100

    slay

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('website.html')
+    return render_template("index.html")
 
-if __name__ == '__main__':
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == "__main__":
     app.run(debug=True)
