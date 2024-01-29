a46fd3696a9db77f8b0f5d91edd673e3146c7a73
Leo T <leo.trygg@elev.ga.ntig.se>
2024-01-29 14:09:23 +0100

    slay

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
+from fetch_img import winter_sales
 
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", image_urls=winter_sales())
 
 @app.route('/about')
 def about():
