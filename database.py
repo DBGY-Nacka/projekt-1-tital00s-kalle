7f7f36e2980bed56d968c13698fff81b05c9e381
Leo T <leo.trygg@elev.ga.ntig.se>
2024-01-31 17:58:17 +0100

    slay

--- database.py
+++ database.py
@@ -1,17 +1,20 @@
 import sqlite3
 
 # Connect to SQLite database (or create it if it doesn't exist)
-conn = sqlite3.connect('webshop.db')
+conn = sqlite3.connect("webshop.db")
 cursor = conn.cursor()
 
 cursor.execute("""
-    CREATE TABLE Products (
-        productid INTEGER PRIMARY KEY,
-        name TEXT NOT NULL,
-        description TEXT,
-        price REAL NOT NULL,
-        inventory_count INTEGER NOT NULL
-    )
+CREATE TABLE Products (
+    productid INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT,
+    price REAL NOT NULL,
+    inventory_count INTEGER NOT NULL,
+    color TEXT,
+    category TEXT NOT NULL,
+    rating INTEGER,
+)
 """)
 
 conn.commit()
