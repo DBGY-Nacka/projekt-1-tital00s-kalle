a87c9d8491eefeba831023b90e1a8be1be9e6570
noah WI <noah.windelhed@elev.ga.ntig.se>
2024-01-31 10:56:26 +0100

    slider funk

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from fetch_img import winter_sales
 
 
@@ -11,6 +11,15 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/slider', methods=['POST'])
+
+def handle_slider():
+    data = request.get_json()
+    slider_value = data['sliderValue']
+    print(slider_value)
+    
+    return slider_value
 
 if __name__ == "__main__":
     app.run(debug=True)
