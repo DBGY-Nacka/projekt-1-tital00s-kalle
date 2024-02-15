8722dd06a65795932227ca9ac5d498cf86e2c464
Leo T <leo.trygg@elev.ga.ntig.se>
2024-02-15 12:29:34 +0100

    slay

--- getdata.py
+++ getdata.py
@@ -5,11 +5,11 @@ def get_images(category):
     cursor = conn.cursor()
     
     if category:
-        cursor.execute("SELECT name, price, image FROM Products WHERE category = ?", (category,))
+        cursor.execute("SELECT name, price, image, productid FROM Products WHERE category = ?", (category,))
     else:
-        cursor.execute("SELECT name, price, image FROM Products")
+        cursor.execute("SELECT name, price, image, productid FROM Products")
 
     images = cursor.fetchall()
-    image_urls =  [(img[0], img[1], img[2]) for img in images]
+    image_urls =  [(img[0], img[1], img[2], img[3]) for img in images]
     conn.close()
     return image_urls
