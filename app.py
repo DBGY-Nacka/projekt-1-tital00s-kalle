ddfd3d066049ff59ea80ec8f6678115157eaeb1b
Lirre-Firre <linus.warnemo@elev.ga.ntig.se>
2024-02-23 12:05:47 +0100

    Updated readme, comments and docstring

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ app = Flask(__name__)
 def home():
     return render_template("index.html", image_paths=get_images(None))
 
-
 app.secret_key = 'your_secret_key'
 
 @app.route('/checkout')
@@ -26,12 +25,30 @@ def products():
 
 @app.route('/slider', methods=['POST'])
 
+
+# Tänk igenom denna funktion. Ska det vara en slider med två värden, eller två sliders
+# med varsitt värde? Gör om docstringen efter det.
 def handle_slider():
+    
+    """
+    Extracts slider values from JSON data received via request,
+    ensures the minimum and maximum slider values are properly assigned,
+    and returns JSON response with corrected slider values.
+
+    Returns:
+    - JSON response containing the corrected minimum and maximum slider values.
+    - HTTP status code 200 indicating a successful request.
+
+    Notes:
+    - Expects JSON data with keys 'minValue' and 'maxValue'.
+    - Assigns 'minValue' to slider_value_min and 'maxValue' to slider_value_max.
+    """
+    
+    
+    
     data = request.get_json()
     slider_value_min = min(data.get('minValue'), data.get('maxValue'))
     slider_value_max = max(data.get('minValue'), data.get('maxValue'))
-
-    print(f"Min Slider Value: {slider_value_min}, Max Slider Value: {slider_value_max}")
     
     return jsonify({"minValue": slider_value_min, "maxValue": slider_value_max}), 200
 
--- readme.txt
+++ readme.txt
@@ -1,10 +1,22 @@
 Product Hub Webiste
 
-This is a webiste for a product shopping company. The purpose is to create a usable website 
-and explore how a website can be made more trustworthy. It solves the issue with many websites feeling insecure 
-for the customer. 
+This is a webiste for a product shopping company. The purpose is to create a website that feels trustworthy, 
+and also is pleasing for each customer. Many of us experience that some webistes are difficult to 
+navigate trough. Therefor we have made a simple yet effective website.
 
-It is developed using html, javascript and css to style the webiste. Combined with python and flask.
+It is developed using html, javascript and css to style the webiste. Combined with python and flask as the framework.
+
+A database is created using SQLite. Each product is stored in the database with the following information:
+
+Product ID: Specifies each product with a unique number
+Name: Product Name
+Description: Product description
+Price: Product Price, used to sort
+Inventory Count: Ammount in stock
+Color: Color, used to sort
+Category: Used to sort by category
+Rating: Customer rating
+Image: Image for each product
 
 Libraries needed to download:
 flask
